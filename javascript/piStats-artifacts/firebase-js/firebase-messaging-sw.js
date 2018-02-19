0102a8df3c6e778662aa95d87d6d92b98f5a8491
priyarajput <36587583+priyarajput@users.noreply.github.com>
2018-02-19 12:56:57 +0530

    javascript files

---
+++ javascript/piStats-artifacts/firebase-js/firebase-messaging-sw.js
@@ -0,0 +1,108 @@
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.2.0/firebase-messaging.js');
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js");
+
+var urlEndoder = new URL(location).searchParams;
+const senderId = urlEndoder.get('senderId');
+const deviceId = urlEndoder.get('deviceId');
+const propertyId = urlEndoder.get('propertyId');
+
+self.addEventListener('periodicsync', function(event) {
+    if (event.registration.tag === "periodicSync") {
+        console.log("Periodic event occurred: ", event);
+    }
+});
+
+firebase.initializeApp({
+    'messagingSenderId': senderId
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(payload => {
+    const title = payload.data.alert;
+    const notificationDate = payload.data.dateOfPush;
+    const options = {
+        body: payload.data.body,
+        icon: payload.data.icon,
+        image: payload.data.image,
+        data: payload.data
+    };
+    console.log("payload", payload);
+
+    dt1 = new Date(notificationDate);
+    dt2 = moment().toDate();
+    if (diff_minutes(dt1, dt2) <= 60) {
+        console.log("Show Notification");
+        return self.registration.showNotification(title, options);
+    }
+});
+
+function diff_minutes(dt1, dt2) {
+    return Math.round((dt2.getTime() - dt1.getTime()) / 60000);
+}
+
+setTimeout(function(){
+    console.log("timeout from onmessage, event is:");
+}, 4000);
+
+self.addEventListener('notificationclick', function (event) {
+    console.log("Notification Clicked",event);
+    event.notification.close();
+    var storyId = event.notification.data.storyId;
+    var language = event.notification.data.language;
+    var urlClick = event.notification.data.clickUrl;
+    var campaignId = event.notification.data.campaignId;
+    var isAd = event.notification.data.isAd;
+    event.waitUntil(self.clients.openWindow(urlClick));
+    if(isAd){
+        var eventJson = {
+            "e":"PageLoad",
+            "pid":propertyId,
+            "c":{
+                "cnt":storyId,
+                "lan":language,
+                "url":urlClick,
+                "ref":{
+                    "t":"pistats",
+                    "o":"ad",
+                    "m":campaignId
+                }
+            },
+            "_dev":{
+                "id":deviceId,
+                "ua":navigator.userAgent,
+                "ctype":"web",
+                "blang":navigator.language || navigator.userLanguage,
+                "v":"2.0",
+                "dist":"Javascript",
+                "loc":{},
+                "d":{
+                    "t":detectDevice(navigator.userAgent)
+                }
+            }
+        };
+        fetch('https://events.pi-stats.com/events/',{
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(eventJson)
+        });
+    }
+});
+
+function detectDevice(userAgent) {
+    var checkmobile = false;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4))) checkmobile = true;
+    (navigator.userAgent||navigator.vendor||window.opera);
+    var checkTablet = false;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4)))
+        checkTablet = true;(navigator.userAgent||navigator.vendor||window.opera);
+    if(checkmobile && checkTablet)
+        return "Mobile";
+    else if(checkTablet&&!checkmobile)
+        return "Tablet";
+    else
+        return "Desktop";
+}
+
+
---
+++ javascript/piStats-artifacts/firebase-js/notification.js
@@ -0,0 +1,197 @@
+importScripts("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js");
+piStats.init(propertyId,false);
+firebase.initializeApp(config);
+var messaging = firebase.messaging();
+var cookieDevice = getCookie("piStatsDEVICEID");
+messaging.onMessage(function(payload) {
+    const title = payload.data.alert;
+    const notificationDate = payload.data.dateOfPush;
+    const options = {
+        body: payload.data.body,
+        icon: payload.data.icon,
+        image: payload.data.image,
+        data: payload.data
+    };
+    if (!("Notification" in window)) {
+        console.log("This browser does not support system notifications");
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+
+        dt1 = new Date(notificationDate);
+        dt2 = moment().toDate();
+        if (diff_minutes(dt1, dt2) <= 60) {
+            console.log("Show Notification");
+            var notification = new Notification(title,options);
+        }
+        notification.onclick = function(event) {
+            event.preventDefault(); // prevent the browser from focusing the Notification's tab
+            var storyId = payload.data.storyId;
+            var urlClick = payload.data.clickUrl;
+            var language = payload.data.language;
+            var isAd = payload.data.isAd;
+            var campaignId = payload.data.campaignId;
+            window.open(urlClick , '_blank');
+            if(isAd){
+                var eventJson = {
+                    "e":"PageLoad",
+                    "pid":propertyId,
+                    "c":{
+                        "cnt":storyId,
+                        "lan":language,
+                        "url":urlClick,
+                        "ref":{
+                            "t":"pistats",
+                            "o":"ad",
+                            "m":campaignId
+                        }
+                    },
+                    "_dev":{
+                        "id":cookieDevice,
+                        "ua":navigator.userAgent,
+                        "ctype":"web",
+                        "blang":navigator.language || navigator.userLanguage,
+                        "v":"2.0",
+                        "dist":"Javascript",
+                        "loc":{},
+                        "d":{
+                            "t":detectDevice(navigator.userAgent)
+                        }
+                    }
+                };
+                fetch('https://events.pi-stats.com/events/',{
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify(eventJson)
+                });
+            }
+            notification.close();
+        }
+    }
+});
+
+function diff_minutes(dt1, dt2) {
+    return Math.round((dt2.getTime() - dt1.getTime()) / 60000);
+}
+
+// Callback fired if Instance ID token is updated.
+messaging.onTokenRefresh(function() {
+    messaging.getToken()
+        .then(function(refreshedToken) {
+            piStats.subscribeNotification(refreshedToken,topic);
+        })
+        .catch(function(err) {
+            console.log('Unable to retrieve refreshed token ', err);
+        });
+});
+
+// On load register service worker
+if ('serviceWorker' in navigator) {
+    var eventJson = {
+                    "pid":propertyId,
+                    "c":{
+                        "cnt":storyId,
+                        "lan":language,
+                        "url":urlClick,
+                        "ref":{
+                            "t":"pistats",
+                            "o":"ad",
+                            "m":campaignId
+                        }
+                    },
+                    "_dev":{
+                        "id":cookieDevice,
+                        "ua":navigator.userAgent,
+                        "ctype":"web",
+                        "blang":navigator.language || navigator.userLanguage,
+                        "v":"2.0",
+                        "dist":"Javascript",
+                        "loc":{},
+                        "d":{
+                            "t":detectDevice(navigator.userAgent)
+                        }
+                    }
+                };
+    var searchParams = new URLSearchParams(window.location.search);
+    window.addEventListener('load', () => {
+            navigator.serviceWorker.register('firebase-messaging-sw.js?senderId='+config.messagingSenderId+'&deviceId='+cookieDevice+'&propertyId='+propertyId).then((registration) => {
+            // Successfully registers service worker
+            messaging.useServiceWorker(registration);
+        })
+        .then(() => {
+            // Requests user browser permission
+            return messaging.requestPermission();
+        })
+        .then(() => {
+            return messaging.getToken();
+
+        })
+        .then((token) =>{
+                console.log("firebase token "+token)
+                piStats.subscribeNotification(token,topic);
+                eventJson["e"]="BrowserNotificationGranted"
+                fetch('https://events.pi-stats.com/events/',{
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify(eventJson)
+                    });
+        })
+        .then((token) =>{
+                setTimeout(function(){ window.close(); }, 5000);
+        })
+        .catch((err) => {
+            if (Notification.permission === 'denied') {
+                eventJson["e"]="BrowserNotificationDeny"
+                fetch('https://events.pi-stats.com/events/',{
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify(eventJson)
+                    });
+            }
+            else if (Notification.permission === 'default') {
+                eventJson["e"]="BrowserNotificationCrossed"
+                fetch('https://events.pi-stats.com/events/',{
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        body: JSON.stringify(eventJson)
+                    });
+            }
+            else{
+                console.log('ServiceWorker registration failed: ', err);
+            }
+            setTimeout(function(){ window.close(); }, 1000);
+        });
+    });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function detectDevice(userAgent) {
+    var checkmobile = false;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4))) checkmobile = true;
+    (navigator.userAgent||navigator.vendor||window.opera);
+    var checkTablet = false;
+    if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(userAgent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(userAgent.substr(0,4)))
+        checkTablet = true;(navigator.userAgent||navigator.vendor||window.opera);
+    if(checkmobile && checkTablet)
+        return "Mobile";
+    else if(checkTablet&&!checkmobile)
+        return "Tablet";
+    else
+        return "Desktop";
+}
